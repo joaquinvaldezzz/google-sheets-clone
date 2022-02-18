abf0f79cf86fd55dc6c10621584670a54b2ea899
John Joaquin Valdez <65848689+joaquin-wakin@users.noreply.github.com>
2022-02-19 07:54:40 +0800

    Add stylelint-config-prettier

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,15 +2,12 @@ const postcssSorting = require('./util/postcssSorting');
 
 module.exports = {
   customSyntax: 'postcss-scss',
-  extends: ['stylelint-config-standard-scss'],
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier'],
   plugins: ['stylelint-order', 'stylelint-scss'],
   rules: {
-    'declaration-colon-newline-after': null,
-    'font-family-name-quotes': 'always-where-recommended',
     'max-nesting-depth': 3,
     'order/order': [...postcssSorting.order],
     'order/properties-alphabetical-order': null,
     'order/properties-order': [...postcssSorting.propertiesOrder],
-    'string-quotes': 'single',
   },
 };
